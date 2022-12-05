2b68d257d4cef2c840cdddfd366d7e94a952d994
sevalyy <seval@yamanyar.com>
2022-12-05 16:15:08 +0100

    show product list after fetching

--- components/ProductList.js
+++ components/ProductList.js
@@ -1,8 +1,35 @@
+import Link from "next/link";
 import React from "react";
+import Image from "next/image";
 
 const ProductList = ({ products }) => {
   console.log(products, "products");
-  return <div>{products && products.map((p) => p.title)} </div>;
+  return (
+    <div>
+      <h1>Webshop </h1>
+      <div className="mt-4 grid grid-cols-1 md:grid-cols-3 gap-8">
+        {products &&
+          products.map((product) => (
+            <div className="  bg-white pt-6 shadow-xl">
+              <Image
+                src={product.image}
+                alt={product.title}
+                width={250}
+                height={150}
+                className="mx-auto w-[350px] h-[200px] object-contain "
+              />
+              <div className="mt-4 px-6  ">
+                <h2>€ {product.price}</h2>
+                <p>{product.title}</p>
+                <button className="bg-black hover:bg-red text-white p-2  block mx-auto my-4 cursor-pointer">
+                  Add to basket
+                </button>
+              </div>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
 };
 
 export default ProductList;
